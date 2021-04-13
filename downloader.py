edd85af6c4d66ec622a2c86eb9f3ba03504c844e
Burak Ayvaz <burak@emipaz.com>
2021-04-13 17:37:40 +0300

    exception error handled

--- downloader.py
+++ downloader.py
@@ -1,15 +1,18 @@
 from pytube import YouTube as yt
 
 def downloadVideos(url):
-	yt(url).streams.get_highest_resolution().download()
-	print(f"Video Title : {yt.title}")
-	print(f"Video Author : {yt.author}")
+	try:
+		yt(url).streams.get_highest_resolution().download()
+		print(f"Video Title : {yt.title}\n")
+		print(f"Video Author : {yt.author}")
+	except Exception:
+		print("Error!")
 print("Welcome Simple Youtube Downloader")
 while True:
 	try:
 		downloadUrl = input("Give Url : ")
 		downloadVideos(str(downloadUrl))
-		print("Download Complete.")
+		print("Complete.")
 	except KeyboardInterrupt:
 		print("\nKeyboard Interrupt.. Exiting")
 		exit()
